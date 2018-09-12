94c39dea7083074da0bd53baeb867a72a8ddb379
Filip <filip.egeric@gmail.com>
2018-09-12 19:43:38 +0200

    Added smooth scroll after submit and localStorage check

--- main.js
+++ main.js
@@ -3,6 +3,20 @@
   let form = document.getElementById("form");
   let submitButton = document.getElementById("submit-button");
   let input = document.getElementById("input");
+  let storedItems = JSON.parse(localStorage.getItem('items'));
+
+  if(storedItems) {
+    let text = '';
+    for(let i = 0; i < storedItems.length; i++) {
+      text += `
+        <div class="todo show">
+          <p>${storedItems[i]}</p>
+          <img class="close" src="close.svg">
+        </div>
+      `;
+    }
+    todos.innerHTML += text;
+  }
 
   form.addEventListener("submit", handleSubmit);
   submitButton.addEventListener("click", handleSubmit);
@@ -10,6 +24,9 @@
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!input.value) {
+      return;
+    }
     let newTodo = document.createElement("div");
     newTodo.className = "todo";
     newTodo.innerHTML = `
@@ -20,6 +37,20 @@
     setTimeout(function() {
       newTodo.className = newTodo.className + " show";
     }, 10);
+
+    // scroll to bottom animation after adding new item
+    setTimeout(() => {
+      let interval = setInterval(() => {
+        todos.scrollTop = todos.scrollTop + 1;
+        if (
+          todos.scrollTop - (todos.scrollHeight - todos.clientHeight) >= -5 &&
+          todos.scrollTop - (todos.scrollHeight - todos.clientHeight) <= 5
+        ) {
+          clearInterval(interval);
+        }
+      }, 2);
+    }, 100);
+
     input.value = "";
     input.focus();
   }
