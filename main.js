be459e084f7bd75fbd9ea10cd745a17566b30bfc
Filip <filip.egeric@gmail.com>
2018-09-12 22:27:24 +0200

    added service worker

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 (function() {
-  let todos = document.getElementById("todos");
-  let form = document.getElementById("form");
-  let submitButton = document.getElementById("submit-button");
-  let input = document.getElementById("input");
+  let todos = document.getElementById('todos');
+  let form = document.getElementById('form');
+  let submitButton = document.getElementById('submit-button');
+  let input = document.getElementById('input');
   let storedItems = JSON.parse(localStorage.getItem('items'));
 
-  if(storedItems) {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service-worker.js').then(() => {
+      console.log('Service Worker Registered');
+    });
+  }
+
+  if (storedItems) {
     let text = '';
-    for(let i = 0; i < storedItems.length; i++) {
+    for (let i = 0; i < storedItems.length; i++) {
       text += `
         <div class="todo show">
           <p>${storedItems[i]}</p>
@@ -20,24 +26,24 @@
     storedItems = [];
   }
 
-  form.addEventListener("submit", handleSubmit);
-  submitButton.addEventListener("click", handleSubmit);
-  todos.addEventListener("click", handleRemove);
+  form.addEventListener('submit', handleSubmit);
+  submitButton.addEventListener('click', handleSubmit);
+  todos.addEventListener('click', handleRemove);
 
   function handleSubmit(e) {
     e.preventDefault();
     if (!input.value) {
       return;
     }
-    let newTodo = document.createElement("div");
-    newTodo.className = "todo";
+    let newTodo = document.createElement('div');
+    newTodo.className = 'todo';
     newTodo.innerHTML = `
       <p>${input.value}</p>
       <img class="close" src="close.svg">
     `;
     todos.appendChild(newTodo);
     setTimeout(function() {
-      newTodo.className = newTodo.className + " show";
+      newTodo.className = newTodo.className + ' show';
     }, 10);
 
     // scroll to bottom animation after adding new item
@@ -56,23 +62,25 @@
     storedItems.push(input.value);
     localStorage.setItem('items', JSON.stringify(storedItems));
 
-    input.value = "";
+    input.value = '';
     input.focus();
   }
 
   function handleRemove(e) {
-    if (e.target && e.target.classList.value === "close") {
+    if (e.target && e.target.classList.value === 'close') {
       e.preventDefault();
-      let index = storedItems.indexOf(e.target.parentElement.children[0].innerHTML);
-      if(index !== -1) {
+
+      let index = storedItems.indexOf(
+        e.target.parentElement.children[0].innerHTML
+      );
+      if (index !== -1) {
         storedItems.splice(index, 1);
         localStorage.setItem('items', JSON.stringify(storedItems));
         console.log(storedItems);
       }
-      
-      
+
       // this actually removes 'show' from class name
-      e.target.parentElement.className = "todo";
+      e.target.parentElement.className = 'todo';
       setTimeout(() => {
         todos.removeChild(e.target.parentElement);
       }, 400);
---
+++ service-worker.js
@@ -0,0 +1,42 @@
+var cacheName = 'TodoApp1.0';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/main.js',
+  '/main.css',
+  '/close.svg'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('Service Worker Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[Sw] Caching app shell!');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if(key !== cacheName) {
+          console.log('[Sw] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Sw] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  )
+});
