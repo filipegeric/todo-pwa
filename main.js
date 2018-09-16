cfad694a4146439a3d5dba147eed7212fb1940ca
Filip <filip.egeric@gmail.com>
2018-09-16 09:07:32 +0200

    Added push notifications support

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 (function() {
   let todos = document.getElementById('todos');
   let form = document.getElementById('form');
@@ -6,17 +8,62 @@
   let storedItems = JSON.parse(localStorage.getItem('items'));
   let sw = null;
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./service-worker.js').then((swReg) => {
-      sw = swReg;
-      console.log('Service Worker Registered');
-    });
+  if ('serviceWorker' in navigator && 'PushManager' in window) {
+    navigator.serviceWorker
+      .register('./service-worker.js')
+      .then(swReg => {
+        sw = swReg;
+        console.log('Service Worker Registered', sw);
+      })
+      .then(() => {
+        sw.pushManager.getSubscription().then(sub => {
+          if (!sub) {
+            subscribeUser();
+          }
+        });
+      });
   }
 
   checkStoredItems();
   form.addEventListener('submit', handleSubmit);
   submitButton.addEventListener('click', handleSubmit);
   todos.addEventListener('click', handleRemove);
+
+  function subscribeUser() {
+    const applicationServerKey = urlB64ToUint8Array(
+      'BCzBS4mgym5ovkb6cIQHyok1yi1HManGSCctWuEiHKefkyWfpxFDD_OvdttgYq3sr3DfmLQG0GJy__ieV9yAnt4'
+    );
+    sw.pushManager
+      .subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: applicationServerKey
+      })
+      .then(subscription => {
+        console.log(JSON.stringify(subscription));
+        // this is just temporary
+        // here subscription should be sent to the server and stored in database
+        let s = composeNewTodo(JSON.stringify(subscription), true);
+        s.setAttribute('style', 'height: 100%; word-wrap: break-word;');
+        s.children[0].setAttribute('style', 'word-wrap: break-word; width: 100%; height: 100%;');
+        todos.appendChild(s);
+        //-----------------------
+      });
+  }
+
+  function urlB64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,7 @@
-var cacheName = 'TodoApp1.1';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/main.js',
-  '/main.css',
-  '/close.svg'
-];
+'use strict';
+
+var cacheName = 'TodoApp1.3';
+var filesToCache = ['/', '/index.html', '/main.js', '/main.css', '/close.svg'];
 
 self.addEventListener('install', function(e) {
   console.log('Service Worker Install');
@@ -21,12 +17,14 @@ self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
     caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if(key !== cacheName) {
-          console.log('[Sw] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
+      return Promise.all(
+        keyList.map(function(key) {
+          if (key !== cacheName) {
+            console.log('[Sw] Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
   return self.clients.claim();
@@ -38,5 +36,22 @@ self.addEventListener('fetch', function(e) {
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
     })
-  )
+  );
+});
+
+self.addEventListener('push', function(e) {
+  const title = "Msg from TODO";
+  const options = {
+    body: e.data.text(),
+    icon: 'icon.png',
+    badge: 'icon.png',
+  };
+
+  e.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(e) {
+  e.notification.close();
+  //e.waitUntil(clients.openWindow('http://127.0.0.1:8887'));
+  e.waitUntil(clients.openWindow('https://todo-pwa-5b853.firebaseapp.com/'));
 });
